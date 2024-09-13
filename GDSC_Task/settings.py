2cee819c88943d91aae65f0b8d99cd8cdc868b34
Dolapo001 <adedolapovictoria927@gmail.com>
2024-09-13 23:59:13 +0100

    commit

--- GDSC_Task/settings.py
+++ GDSC_Task/settings.py
@@ -141,7 +141,7 @@ load_dotenv()  # Load environment variables from .env file
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_gdsc_task2',
+        'NAME': 'test_gdsc_task',
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
